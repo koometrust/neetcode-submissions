827d6887ce9dec95d4a4af2bb23bf9da9ea567bb
koomeJude <126925510+koometrust@users.noreply.github.com>
2026-07-12 00:37:29 +0300

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,42 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+     #hasmap for frequencies
+     #put kv pairs in a list
+     #sort the list according to frequencies
+     #make results array
+     #pop from soter list and put in results list
+     #return results
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        # toSort = []
+        # for num, freq in count.items():
+        #     toSort.append([freq,num])
+        # toSort.sort() #.sort() modifies the original and returnd non Sorted returns a new 
+
+        # res = []
+        # while len(res) < k:
+        #     res.append(toSort.pop()[1])
+        # return res
+
+#Using a Heap (min-heap)
+        heap = []
+        for num in count.keys(): #why do we use .keys and when we push why do we always us the freq, num format, only hash map uses the num , freq format
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        res = []
+        for num in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+        
+    
+
+            
+   
+
+
+
+
