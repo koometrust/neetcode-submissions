7f21464691265be9fc393d90aeb9fd6bd8d6a7b5
koomejude <126925510+koometrust@users.noreply.github.com>
2026-08-17 23:44:42 +0300

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+
+        for word in strs:
+                newWord = ''.join(sorted(word))
+                # anagrams[newWord].append(word)
+                anagrams[newWord] = [word] + anagrams.get(newWord, [])
+
+
+        return list(anagrams.values())
+
+        # wordCount = defaultdict(list)
+        # for word in strs:
+        #    sortedW = ''.join(sorted(word)) #why this extra ''.join
+        #    wordCount[sortedW].append(word)
+        
+        # return list(wordCount.values())
+
+                # wordCount[word] = 1 + wordCount.get(word, 0)
+
+
+        
+        
+
