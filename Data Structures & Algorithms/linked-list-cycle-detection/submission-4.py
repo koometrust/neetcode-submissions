70171aed57baae36281dd632e6ce040b2c9e81f7
koomejude <126925510+koometrust@users.noreply.github.com>
2026-08-20 23:55:23 +0300

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.py
@@ -0,0 +1,111 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        # Input: head = [1,2,3,4], index = 1
+
+        # Output: true
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+
+              
+        return False 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # floyds hare and tortoise
+        # # hare x2
+        # # tor 
+
+        # slow, fast = head, head  
+
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+
+        #     if slow == fast: #really cool btw
+        #         return True
+             
+
+        # return False
+        
+        # return head.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # slow, fast = head, head
+
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        #     if fast == slow:
+        #         return True
+        # what would happen if we had a return false after return true
+        # return False
