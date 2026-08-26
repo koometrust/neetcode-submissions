eacb6655b1d55a4ceb50f712d1f1b5bf6819173c
koomejude <126925510+koometrust@users.noreply.github.com>
2026-08-26 03:20:46 +0300

    Add: longest-repeating-substring-with-replacement - submission-13

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-13.py
@@ -0,0 +1,82 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        # Input: s = "XYYX", k = 2
+
+            # Output: 4
+            if not s:
+                return 0
+
+            longestSub = 0
+
+            l = 0
+            counter = {}
+
+            for r in range(len(s)):
+                # counter[l] = 1 + counter.get(l, 0)
+                counter[s[r]] = 1 + counter.get(s[r], 0)
+
+                # if len 0f sub - most common value freq > k = reduce window
+
+                if (r-l+1) - max(counter.values()) <= k:
+                    # r+=1
+                    longestSub = max(longestSub, r-l+1)
+                else:
+                    counter[s[l]] -= 1
+                    l+=1
+            
+            return longestSub
+
+
+
+            # for each we record the longest substring length using max
+
+
+            # else if len 0f sub - most common value freq < k = increse window.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # counter = {}
+        # l = 0
+        # lSeq = 0
+
+        # # for r in range(l, len(s)):
+        # for r in range(len(s)):
+
+        #     counter[s[r]] = 1 + counter.get(s[r] , 0)
+        #     # counter[r] = counter.get(r , 0)
+
+        #     #while invalid: shrink
+        #     while (r-l + 1) - max(counter.values()) > k:
+        #         counter[s[l]] -= 1
+        #         l+=1
+        #     lSeq = max(lSeq, r-l+1)
+
+
+        # return lSeq
+
+
+
+        
