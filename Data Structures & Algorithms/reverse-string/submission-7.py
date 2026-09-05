725bf99a047d63b2544d9ba0f137c533098d707d
koomejude <126925510+koometrust@users.noreply.github.com>
2026-09-06 00:39:24 +0300

    Add: reverse-string - submission-7

---
+++ Data Structures & Algorithms/reverse-string/submission-7.py
@@ -0,0 +1,51 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+
+        # s = ["n","e","e","t"]
+
+        # ["t","e","e","n"]
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            #switch the left and right values
+            store = s[left]
+            s[left] = s[right]
+            s[right] = store
+            #move the pointers inwards
+            left += 1
+            right -= 1
+        
+        return s
+
+            
+
+
+
+
+
+
+
+
+
+    
+    
+    # \
+    # \
+    # \
+    # \
+    # \
+    # \
+
+        
+        # reversed = []
+        # while s:
+        #     reversed.append(s.pop())
+
+        # s[:] = reversed
+        
+        # # return s
