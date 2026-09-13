9ebf68690c2228c0f7f51df9a1ed697671b7909c
koomejude <126925510+koometrust@users.noreply.github.com>
2026-09-13 14:34:45 +0300

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.py
@@ -0,0 +1,57 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #  nums = [1,2,3,4,4]
+        #                |   |
+        # [2,10,30,35,35,35]
+        #        |        |
+
+        # first, dupFinder = 0, 1
+        # while dupFinder <= len(nums) - 1:
+        #     if nums[first] != nums[dupFinder]:
+        #         dupFinder += 1
+        #         first +=1
+        #     if nums[first] == nums[dupFinder]: 
+        #         dupFinder += 1
+        #     else:
+        #         nums1[first + 1] = nums[dupFinder]
+
+        #     #change nums1[first + 1] = nums[dupFinder]
+
+        first, dupFinder = 1, 1
+        # [2,10,30,35,35,35]
+        #     f 
+        #       d
+
+        # while dupFinder <= len(nums)-1:
+        #     if nums[first] == nums[dupFinder]:
+        #         dupFinder += 1
+        #     elif nums[first] != nums[dupFinder]:
+        #         nums[first] = nums[dupFinder]
+        #         first += 1
+        #         dupFinder += 1
+
+        # return first
+
+        while dupFinder <= len(nums)-1:
+            if nums[dupFinder] != nums[dupFinder - 1]:
+                nums[first] = nums[dupFinder]
+                first += 1            
+            dupFinder += 1
+
+        return first
+
+            
+
+
+
+
+
+
+            
+
+
+
+
+
+
+       
